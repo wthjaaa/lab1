1152f999f66f5965d2c6903315077377921a2fc1
salam <salam.s.boustaji@gmail.com>
2025-01-23 15:59:58 +0100

    Saab95 inherted

--- Saab95mod.java
+++ Saab95mod.java
@@ -1,2 +1,21 @@
-public class Saab95mod {h
+import  java.awt.*;
+
+public class Saab95mod extends Car {
+    private boolean turboOn;
+
+    public Saab95mod() {
+        super(2,125,Color.red, "Saab95");
+        turboOn = false;
+    }
+    public void setTurboOn() {
+        turboOn = true;
+    }
+    public void setTurboOff() {
+        turboOn = false;
+    }
+    @Override
+    public double speedFactor() {
+        double turbo = turboOn ? 1.3 : 1;
+        return getEnginePower() * 0.01 * turbo;
+    }
 }
