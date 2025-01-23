71bfcb0dece5ba624864ac1a08cf5561a3f0ce28
hussam <hussambadranalbasha@gmail.com>
2025-01-23 10:26:19 +0100

    Abstract class

---
+++ Car.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public abstract class Car {
+
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+
+    public Car(int nrDoors, double enginePower, Color color, String modelName){
+
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        stopEngine();
+    }
