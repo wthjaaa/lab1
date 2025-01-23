365e2e938f6e4df0356e55f9e0fab6ee5ca745a3
salam <salam.s.boustaji@gmail.com>
2025-01-23 16:40:09 +0100

    Parent Class + Movable

--- Car.java
+++ Car.java
@@ -8,13 +8,56 @@ public abstract class Car implements Movable{
     private Color color; // Color of the car
     private String modelName; // The car model name
 
+    private double x; // car's position on x-axis
+    private double y; // car's position on y-axis
+    private int direction; // 0=North 1=East 2=South 3=West
+
     public Car(int nrDoors, double enginePower, Color color, String modelName){
 
         this.nrDoors = nrDoors;
         this.enginePower = enginePower;
         this.color = color;
         this.modelName = modelName;
+        this.x = 0; // Start position x
+        this.y = 0; // Start position y
+        this.direction = 0; // Start direction North
         stopEngine();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public void move() {
+        switch (direction) {
+            case 0: // North
+                y += currentSpeed;
+                break;
+            case 1: // East
+                x += currentSpeed;
+                break;
+            case 2: // South
+                y -= currentSpeed;
+                break;
+            case 3: // West
+                x -= currentSpeed;
+                break;
+        }
+    }
+
+    @Override
+    public void turnLeft() {
+        direction = (direction - 1 + 4) % 4;
+    }
+
+    @Override
+    public void turnRight() {
+        direction = (direction + 1) % 4;
     }
 
     public int getNrDoors(){
