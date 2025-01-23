c2b911378e070afde3e107ba967a43f57867759f
salam <salam.s.boustaji@gmail.com>
2025-01-23 16:40:41 +0100

    Subclass Saab

--- Saab95mod.java
+++ Saab95mod.java
@@ -4,18 +4,23 @@ public class Saab95mod extends Car {
     private boolean turboOn;
 
     public Saab95mod() {
-        super(2,125,Color.red, "Saab95");
+        super(2, 125, Color.red, "Saab95");
         turboOn = false;
     }
+
     public void setTurboOn() {
         turboOn = true;
     }
+
     public void setTurboOff() {
         turboOn = false;
     }
+
     @Override
     public double speedFactor() {
         double turbo = turboOn ? 1.3 : 1;
         return getEnginePower() * 0.01 * turbo;
     }
+
 }
+
